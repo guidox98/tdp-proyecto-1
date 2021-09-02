999a015c40400d515ee130354337f1af2c91a99f
guidox98 <guidox98@gmail.com>
2021-09-02 08:37:35 -0300

    info del estudiante y tiempo de creacion en pantalla

--- src/gui/SimplePresentationScreen.java
+++ src/gui/SimplePresentationScreen.java
@@ -1,5 +1,9 @@
 package gui;
 
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 
@@ -9,6 +13,13 @@ import javax.swing.border.EmptyBorder;
 
 import entities.Student;
 import javax.swing.JTabbedPane;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import java.awt.GridLayout;
+import javax.swing.SwingConstants;
 
 @SuppressWarnings("serial")
 public class SimplePresentationScreen extends JFrame {
@@ -17,6 +28,16 @@ public class SimplePresentationScreen extends JFrame {
 	private JPanel tabInformation;
 	private JTabbedPane tabbedPane;
 	private Student studentData;
+	private JLabel ApellidoLabel;
+	private JLabel NombreLabel;
+	private JLabel EmailLabel;
+	private JLabel GitHubLabel;
+	private JTextField LUTextField;
+	private JTextField ApellidoTextField;
+	private JTextField NombreTextField;
+	private JTextField EmailTextField;
+	private JTextField GitHubTextField;
+	private JLabel TiempoLabel;
 
 	public SimplePresentationScreen(Student studentData) {
 		this.studentData = studentData;
@@ -25,9 +46,9 @@ public class SimplePresentationScreen extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		
-		setTitle("TdP-DCIC-UNS 2021 :: Pantalla de presentaciÃ³n");
+		setTitle("TdP-DCIC-UNS 2021 :: Pantalla de presentación");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(new Dimension(615, 250));
+		setSize(new Dimension(615, 262));
 		setResizable(false);
 		setContentPane(contentPane);
 		
@@ -35,11 +56,95 @@ public class SimplePresentationScreen extends JFrame {
 	}
 	
 	private void init() {
+		
+		DateTimeFormatter fecha = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        DateTimeFormatter hora = DateTimeFormatter.ofPattern("HH:mm:ss");
+        
 		// Tabbed Pane to student personal data
 		tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		tabInformation = new JPanel();
 		tabInformation.setPreferredSize(new Dimension(425, 275));
-		tabbedPane.addTab("InformaciÃ³n del alumno", null, tabInformation, "Muestra la informaciÃ³n declarada por el alumno");
+		tabbedPane.addTab("Información del alumno", null, tabInformation, "Muestra la información declarada por el alumno");
+		
+		JLabel LULabel = new JLabel("LU");
+		
+		LUTextField = new JTextField();
+		LUTextField.setText(""+ studentData.getId());
+		LUTextField.setColumns(10);
+		
+		ApellidoLabel = new JLabel("Apellido");
+		
+		ApellidoTextField = new JTextField();
+		ApellidoTextField.setText(studentData.getLastName());
+		ApellidoTextField.setColumns(10);
+		
+		NombreLabel = new JLabel("Nombre");
+		
+		NombreTextField = new JTextField();
+		NombreTextField.setText(studentData.getFirstName());
+		NombreTextField.setColumns(10);
+		
+		EmailLabel = new JLabel("Email");
+		
+		EmailTextField = new JTextField();
+		EmailTextField.setText(studentData.getMail());
+		EmailTextField.setColumns(10);
+		
+		GitHubLabel = new JLabel("GitHub URL");
+		
+		GitHubTextField = new JTextField();
+		GitHubTextField.setText(studentData.getGithubURL());
+		GitHubTextField.setColumns(10);
+		GroupLayout gl_tabInformation = new GroupLayout(tabInformation);
+		gl_tabInformation.setHorizontalGroup(
+			gl_tabInformation.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_tabInformation.createSequentialGroup()
+					.addGap(20)
+					.addGroup(gl_tabInformation.createParallelGroup(Alignment.LEADING, false)
+						.addComponent(GitHubLabel, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addComponent(EmailLabel, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addComponent(NombreLabel, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addComponent(ApellidoLabel, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addComponent(LULabel, GroupLayout.PREFERRED_SIZE, 58, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(gl_tabInformation.createParallelGroup(Alignment.LEADING)
+						.addComponent(LUTextField, GroupLayout.DEFAULT_SIZE, 313, Short.MAX_VALUE)
+						.addComponent(NombreTextField, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 281, Short.MAX_VALUE)
+						.addComponent(GitHubTextField, GroupLayout.DEFAULT_SIZE, 313, Short.MAX_VALUE)
+						.addComponent(ApellidoTextField, Alignment.TRAILING)
+						.addComponent(EmailTextField))
+					.addGap(10))
+		);
+		gl_tabInformation.setVerticalGroup(
+			gl_tabInformation.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_tabInformation.createSequentialGroup()
+					.addGap(1)
+					.addGroup(gl_tabInformation.createParallelGroup(Alignment.BASELINE)
+						.addComponent(LULabel, GroupLayout.PREFERRED_SIZE, 27, GroupLayout.PREFERRED_SIZE)
+						.addComponent(LUTextField, GroupLayout.PREFERRED_SIZE, 17, GroupLayout.PREFERRED_SIZE))
+					.addGap(7)
+					.addGroup(gl_tabInformation.createParallelGroup(Alignment.BASELINE)
+						.addComponent(ApellidoLabel, GroupLayout.PREFERRED_SIZE, 27, GroupLayout.PREFERRED_SIZE)
+						.addComponent(ApellidoTextField, GroupLayout.PREFERRED_SIZE, 16, GroupLayout.PREFERRED_SIZE))
+					.addGap(7)
+					.addGroup(gl_tabInformation.createParallelGroup(Alignment.BASELINE)
+						.addComponent(NombreLabel, GroupLayout.PREFERRED_SIZE, 27, GroupLayout.PREFERRED_SIZE)
+						.addComponent(NombreTextField, GroupLayout.PREFERRED_SIZE, 17, GroupLayout.PREFERRED_SIZE))
+					.addGap(7)
+					.addGroup(gl_tabInformation.createParallelGroup(Alignment.BASELINE)
+						.addComponent(EmailLabel, GroupLayout.PREFERRED_SIZE, 27, GroupLayout.PREFERRED_SIZE)
+						.addComponent(EmailTextField, GroupLayout.PREFERRED_SIZE, 16, GroupLayout.PREFERRED_SIZE))
+					.addGap(7)
+					.addGroup(gl_tabInformation.createParallelGroup(Alignment.BASELINE)
+						.addComponent(GitHubLabel, GroupLayout.PREFERRED_SIZE, 27, GroupLayout.PREFERRED_SIZE)
+						.addComponent(GitHubTextField, GroupLayout.PREFERRED_SIZE, 18, GroupLayout.PREFERRED_SIZE))
+					.addContainerGap())
+		);
+		tabInformation.setLayout(gl_tabInformation);
 		contentPane.add(tabbedPane, BorderLayout.WEST);
+		
+		// pide la fecha y el tiempo local con un formato dd/mm/aaaa y hh:mm:ss y lo muestra debajo de todo en el "sur"
+		TiempoLabel = new JLabel("Esta ventana fue generada el "+ LocalDate.now().format(fecha) +" a las " + LocalTime.now().format(hora));
+		contentPane.add(TiempoLabel, BorderLayout.SOUTH);
 	}
 }
--- src/launcher/Launcher.java
+++ src/launcher/Launcher.java
@@ -1,12 +1,30 @@
 package launcher;
 
-public class Launcher {
+import entities.Student;
+import gui.SimplePresentationScreen;
+
+
+public class Launcher {	
+	
+	
 	public static void main(String [] args) {
         //Schedule a job for the event-dispatching thread:
         //creating and showing this application's GUI.
+		int id = 114691;
+		String apellido = "Pierdominici";
+		String nombre = "Guido";
+		String mail = "guidopier@live.com.ar";
+		String githubURL = "https://github.com/guidox98";
+		String foto = "123";
+		Student estudiante = new Student(id, apellido, nombre, mail, githubURL,foto);
+		
+       
+		
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
             	
+            	 SimplePresentationScreen sps = new SimplePresentationScreen(estudiante);
+            	 sps.setVisible(true);
             }
         });
     }
